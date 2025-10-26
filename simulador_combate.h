9ddbc4ab9dcdc2629aa54240269b06c2fb75d5e1
TomasB <tomasbizz2020@gmail.com>
2025-10-26 19:56:52 -0300

    Cambio de logica_desiciones a simular_desiciones.c

--- simulador_combate.h
+++ simulador_combate.h
@@ -22,6 +22,5 @@ int atacar(personaje_t entidad_1, personaje_t entidad_2);
 int curar(personaje_t entidad);
 
 void imprimir_estado(personaje_t entidad_1, personaje_t entidad_2);
-void logica_turnos(personaje_t entidad_1, personaje_t entidad_2);
 
 #endif
--- simulador_desiciones.h
+++ simulador_desiciones.h
@@ -6,5 +6,6 @@
 #include <string.h>
 
 void decidir_accion(personaje_t entidad, personaje_t oponente);
+void logica_turnos(personaje_t entidad_1, personaje_t entidad_2);
 
 #endif
